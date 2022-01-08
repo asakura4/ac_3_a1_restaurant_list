ee335026d3e283ef0450cba91b2ea5a437368b26
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-01-08 00:48:29 -0800

    feature: add edit, detail, delete btm and implement delete

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const path = require('path');
+
 const Restaurant = require('./models/restaurant');
 
 const app = express();
@@ -8,10 +11,10 @@ const app = express();
 require('./config/mongoose');
 const PORT = 3000;
 
-const { restaurants } = require('./models/sampleData/data');
-
 app.engine('hbs',  exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 // CSS file setting 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,6 +28,16 @@ app.get('/', (req, res) => {
         .then(restaurants => res.render('index', {restaurants}))
         .catch(error => console.error(error));
 })
+
+// app.post('/restaurants', (req, res) => {
+
+
+// })
+
+app.get('/restaurants/new', (req, res) => {
+    return res.render('new');
+});
+
 
 app.get('/restaurants/search', (req, res) => {
     const keyword = req.query.keyword;
@@ -46,6 +59,34 @@ app.get('/restaurants/:id', (req, res) => {
     return Restaurant.findById(id)
         .lean()
         .then(restaurant => res.render('show', { restaurant }))
+        .catch(error => console.log(error));
+})
+
+// app.get('/restaurants/:id/edit', (req, res) => {
+//     const id = req.params.id;
+//     return Restaurant.findById(id)
+//         .lean()
+//         .then(restaurant => res.render('edit', { restaurant }))
+//         .catch(error => console.log(error));
+// })
+
+// app.put('/restaurants/:id', (req, res) => {
+//     const id = req.params.id
+//     const { name, isDone } = req.body
+  
+//     return Restaurant.findById(id)
+//       .then(restaurant => {
+//         return restaurant.save()
+//       })
+//       .then(() => res.redirect(`/restaurant/${id}`))
+//       .catch(error => console.log(error))
+// })
+
+app.delete('/restaurants/:id', (req, res) => {
+    const id = req.params.id;
+    return Restaurant.findById(id)
+        .then(restaurant => restaurant.remove())
+        .then(() => res.redirect('/'))
         .catch(error => console.log(error));
 })
 
