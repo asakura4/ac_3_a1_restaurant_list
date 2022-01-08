ecb876b059269d094379dc7599b5a15b73149235
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-01-08 14:55:31 -0800

    feature: add login page

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const path = require('path')
+require('./config/mongoose')
 
-const Restaurant = require('./models/restaurant')
 
-const route = require('./routes')
+const routes = require('./routes')
 const app = express()
 
-require('./config/mongoose')
 const PORT = 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
@@ -18,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // CSS file setting
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(route)
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(routes)
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`App is running on localhost:${PORT}`)
